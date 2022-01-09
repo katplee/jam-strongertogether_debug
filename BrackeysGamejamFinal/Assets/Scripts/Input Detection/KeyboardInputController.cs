ae29c2ec8470cd1b79fd75ba87a3b3c18e0373bc
katplee <katpelee@gmail.com>
2022-01-09 17:46:53 +0900

    Files updated (batch #9)
    No errors.

--- BrackeysGamejamFinal/Assets/Scripts/Input Detection/KeyboardInputController.cs
+++ BrackeysGamejamFinal/Assets/Scripts/Input Detection/KeyboardInputController.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class KeyboardInputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private KeyboardInputs keyboardInputs;
+
+    //Game objects with input controls
+
+    private void Awake()
     {
-        
+        keyboardInputs = new KeyboardInputs();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
     }
+
+    private void OnEnable()
+    {
+        keyboardInputs.Enable();
+
+        //press R to toggle the tame menu on and off
+        keyboardInputs.TameMenu.PressR.performed += ShowTameMenu;
+    }
+
+    private void ShowTameMenu(InputAction.CallbackContext obj)
+    {
+        UITameMenu.Instance.UpdateTameMenuButtons();
+        UITameMenu.Instance.ToggleVisibility();
+    }
+
+    private void OnDisable()
+    {
+        keyboardInputs.Disable();
+    }
+
 }
+
--- BrackeysGamejamFinal/Assets/Scripts/Panels/Tame Menu/UI/UITameButton.cs
+++ BrackeysGamejamFinal/Assets/Scripts/Panels/Tame Menu/UI/UITameButton.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UITameButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Button button;
+
+    private InventoryData inventory = new InventoryData();
+    private DragonType type;
+
+    private void Awake()
     {
-        
+        SubscribeEvents();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        UnsubscribeEvents();
+    }
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+
+        //set the type of dragon in the panel tile
+        SetDragonType();
+
+        //do not show dragon icon if no tamed dragons of the type has been tamed
+        SetInteractability();
+    }
+
+    private void SetInteractability()
+    { 
+        //set the interactability of the button
+        InventorySave inventorySave = InventorySave.Instance.LoadInventoryData();
+        inventory = inventorySave.inventory;
+
+        if (inventory.CountTamedDragons(type) > 0)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void SetDragonType()
+    {
+        int found = tag.IndexOf("Dragon");
+        string element = tag.Substring(0, found).ToUpper();
+        type = (DragonType)Enum.Parse(typeof(DragonType), element);
+    }
+
+    private void SubscribeEvents()
+    {
+        UITameMenu.OnTameMenuDisplay += SetInteractability;
+    }
+
+    private void UnsubscribeEvents()
+    {
+        UITameMenu.OnTameMenuDisplay -= SetInteractability;
     }
 }
